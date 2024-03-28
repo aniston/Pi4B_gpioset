8b1c4691af2b131ad5123e7028e6f9af403f56c0
Todor Velev <todor.velev@rakwireless.com>
2024-03-28 14:10:29 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@
 SX1301_RESET_BCM_PIN=17
 
 # Export the GPIO pin
-gpioset gpiochip0 $SX1301_RESET_BCM_PIN=0
-gpioset gpiochip0 $SX1301_RESET_BCM_PIN=1
-gpioset gpiochip0 $SX1301_RESET_BCM_PIN=0
+gpioset gpiochip4 $SX1301_RESET_BCM_PIN=0
+gpioset gpiochip4 $SX1301_RESET_BCM_PIN=1
+gpioset gpiochip4 $SX1301_RESET_BCM_PIN=0
 
 # Run other commands
 ./set_eui.sh
